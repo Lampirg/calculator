4ed07dd54ab8eec8b28ef623052ed83e72a60bcf
Lampirg <63813365+Lampirg@users.noreply.github.com>
2023-07-25 23:36:53 +0300

    Initial commit

---
+++ src/test/java/com/lampirg/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lampirg.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
