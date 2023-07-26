d433455be0bf1f4ecf7081ed0c0330fe742331a2
Lampirg <63813365+Lampirg@users.noreply.github.com>
2023-07-26 17:40:33 +0300

    Create Parser
    And inject it into SimpleCalculator.

--- src/main/java/com/lampirg/calculator/logic/SimpleCalculator.java
+++ src/main/java/com/lampirg/calculator/logic/SimpleCalculator.java
@@ -2,15 +2,26 @@ package com.lampirg.calculator.logic;
 
 import com.lampirg.calculator.logic.expression.*;
 import com.lampirg.calculator.logic.expression.number.*;
+import com.lampirg.calculator.logic.parse.Parser;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.text.DecimalFormat;
 import java.util.*;
 import java.util.function.BiFunction;
 
+@Service
 public class SimpleCalculator implements Calculator<String, String> {
+
+    private Parser parser;
+
+    @Autowired
+    public void setParser(Parser parser) {
+        this.parser = parser;
+    }
 
     private Map<String, BiFunction<Double, Double, BinaryNumberExpression>> expressionMap = Map.of(
             "+", Sum::new,
---
+++ src/main/java/com/lampirg/calculator/logic/parse/Parser.java
@@ -0,0 +1,8 @@
+package com.lampirg.calculator.logic.parse;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Parser {
+
+}
