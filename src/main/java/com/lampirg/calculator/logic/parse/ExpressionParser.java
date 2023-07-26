cdf29216a20254c25996abc244046b5aa91f84bb
Lampirg <63813365+Lampirg@users.noreply.github.com>
2023-07-26 19:09:31 +0300

    Create ExpressionParser

---
+++ src/main/java/com/lampirg/calculator/logic/parse/ExpressionParser.java
@@ -0,0 +1,81 @@
+package com.lampirg.calculator.logic.parse;
+
+import com.lampirg.calculator.logic.expression.DoubleExpression;
+import com.lampirg.calculator.logic.expression.number.*;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.BiFunction;
+
+@Service
+public class ExpressionParser {
+
+    private Map<String, BiFunction<Double, Double, BinaryNumberExpression>> expressionMap = Map.of(
+            "+", Sum::new,
+            "-", Subtract::new,
+            "*", Multiply::new,
+            "/", Divide::new
+    );
+
+    public DoubleExpression parse(String inputExpression) {
+        Iterator it = new Iterator(0);
+        DoubleExpression expression = firstParseBinaryExpression(inputExpression, it);
+        while (it.getIndex() < inputExpression.length()) {
+            expression = parseBinaryExpression(expression, inputExpression, it);
+        }
+        return expression;
+    }
+
+    private DoubleExpression firstParseBinaryExpression(String expression, Iterator it) {
+        return parseBinaryExpression(null, expression, it);
+    }
+
+    private DoubleExpression parseBinaryExpression(DoubleExpression previousExpression, String expression, Iterator it) {
+        List<Double> numbers = initializeNumberList(previousExpression);
+        Optional<String> operator = Optional.empty();
+        for (; it.getIndex() < expression.length() && numbers.size() < 2; it.increment()) {
+            if (isDigitOrLeadingNegative(expression, it.getIndex())) {
+                numbers.add(parseNumber(expression, it));
+                continue;
+            }
+            if (isOperator(expression.charAt(it.getIndex())))
+                operator = Optional.of(String.valueOf(expression.charAt(it.getIndex())));
+        }
+        return expressionMap.get(operator.orElseThrow()).apply(numbers.get(0), numbers.get(1));
+    }
+
+    private List<Double> initializeNumberList(DoubleExpression previousExpression) {
+        List<Double> numbers = new ArrayList<>();
+        Optional.ofNullable(previousExpression).ifPresent(doubleExpression -> numbers.add(doubleExpression.compute()));
+        return numbers;
+    }
+
+    private boolean isDigitOrLeadingNegative(String expression, int i) {
+        return Character.isDigit(expression.charAt(i)) || (expression.charAt(i) == '-' && i == 0);
+    }
+
+    private boolean isOperator(char ch) {
+        return !Character.isDigit(ch);
+    }
+
+    private double parseNumber(String expression, Iterator it) {
+        int sign = 1;
+        if (expression.charAt(it.getIndex()) == '-') {
+            sign = -1;
+            it.increment();
+        }
+        int j = it.getIndex();
+        while (j < expression.length() && isDigitOrComa(expression, j))
+            j++;
+        double number = Double.parseDouble(expression.substring(it.getIndex(), j)) * sign;
+        it.setIndex(j - 1);
+        return number;
+    }
+
+    private boolean isDigitOrComa(String expression, int j) {
+        return Character.isDigit(expression.charAt(j)) || expression.charAt(j) == '.';
+    }
+}
---
+++ src/test/java/com/lampirg/calculator/unit/TestExpressionParser.java
@@ -0,0 +1,65 @@
+package com.lampirg.calculator.unit;
+
+import com.lampirg.calculator.logic.expression.number.Divide;
+import com.lampirg.calculator.logic.expression.number.Multiply;
+import com.lampirg.calculator.logic.expression.number.Subtract;
+import com.lampirg.calculator.logic.expression.number.Sum;
+import com.lampirg.calculator.logic.parse.ExpressionParser;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestExpressionParser {
+    
+    private ExpressionParser expressionParser = new ExpressionParser();
+    
+    @Test
+    void givenSimplestExpressions() {
+        Assertions.assertEquals(new Sum(2, 2), expressionParser.parse("2+2"));
+        Assertions.assertEquals(new Subtract(2, 2), expressionParser.parse("2-2"));
+        Assertions.assertEquals(new Multiply(4, 2), expressionParser.parse("4*2"));
+        Assertions.assertEquals(new Divide(8, 2), expressionParser.parse("8/2"));
+    }
+
+    @Test
+    void givenMinusAtBeginning() {
+        Assertions.assertEquals(new Sum(-2, 2), expressionParser.parse("-2+2"));
+        Assertions.assertEquals(new Subtract(-2, 2), expressionParser.parse("-2-2"));
+        Assertions.assertEquals(new Multiply(-4, 2), expressionParser.parse("-4*2"));
+        Assertions.assertEquals(new Divide(-8, 2), expressionParser.parse("-8/2"));
+    }
+
+    @Test
+    void givenNotDigitNumbers() {
+        Assertions.assertEquals(new Sum(12, 24), expressionParser.parse("12+24"));
+        Assertions.assertEquals(new Subtract(25, 11), expressionParser.parse("25-11"));
+        Assertions.assertEquals(new Multiply(15, 10), expressionParser.parse("15*10"));
+        Assertions.assertEquals(new Divide(125, 25), expressionParser.parse("125/25"));
+    }
+
+    @Test
+    void givenNotDigitNumbersWithMinus() {
+        Assertions.assertEquals(new Sum(-12, 24), expressionParser.parse("-12+24"));
+        Assertions.assertEquals(new Subtract(-25, 11), expressionParser.parse("-25-11"));
+        Assertions.assertEquals(new Multiply(-15, 10), expressionParser.parse("-15*10"));
+        Assertions.assertEquals(new Divide(-125, 25), expressionParser.parse("-125/25"));
+    }
+
+    @Test
+    void givenNonInteger() {
+        Assertions.assertEquals(new Divide(5, 2), expressionParser.parse("5/2"));
+        Assertions.assertEquals(new Sum(2.5, 2), expressionParser.parse("2.5+2"));
+        Assertions.assertEquals(new Subtract(2.5, 2.5), expressionParser.parse("2.5-2.5"));
+        Assertions.assertEquals(new Multiply(2, 2.5), expressionParser.parse("2*2.5"));
+        Assertions.assertEquals(new Divide(9.74, 9.74), expressionParser.parse("9.74/9.74"));
+    }
+
+    @Test
+    void givenNonIntegerWithLeadingNegative() {
+        Assertions.assertEquals(new Divide(-5, 2), expressionParser.parse("-5/2"));
+        Assertions.assertEquals(new Sum(-2.5, 2), expressionParser.parse("-2.5+2"));
+        Assertions.assertEquals(new Subtract(-2.5, 2.5), expressionParser.parse("-2.5-2.5"));
+        Assertions.assertEquals(new Multiply(-2, 2.5), expressionParser.parse("-2*2.5"));
+        Assertions.assertEquals(new Divide(-9.74, 9.74), expressionParser.parse("-9.74/9.74"));
+    }
+
+}
