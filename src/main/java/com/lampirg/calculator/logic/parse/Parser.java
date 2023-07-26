8b651838e3bdd90f08a3f07d5340ae5498588e3d
Lampirg <63813365+Lampirg@users.noreply.github.com>
2023-07-26 20:44:43 +0300

    Refactor: delegate logic to ExpressionParser

--- src/main/java/com/lampirg/calculator/logic/parse/Parser.java
+++ src/main/java/com/lampirg/calculator/logic/parse/Parser.java
@@ -7,14 +7,17 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 import java.util.function.BiFunction;
 
 @Service
 public class Parser {
+
+    private ExpressionParser expressionParser;
+
+    public Parser(ExpressionParser expressionParser) {
+        this.expressionParser = expressionParser;
+    }
 
     private Map<String, BiFunction<Double, Double, BinaryNumberExpression>> expressionMap = Map.of(
             "+", Sum::new,
@@ -24,76 +27,31 @@ public class Parser {
     );
 
     public DoubleExpression parse(String inputExpression) {
-        Iterator it = new Iterator(0);
-        DoubleExpression expression = firstParseBinaryExpression(inputExpression, it);
-        while (it.getIndex() < inputExpression.length()) {
-            expression = parseBinaryExpression(expression, inputExpression, it);
-        }
-        return expression;
+        inputExpression = deBracketize(inputExpression);
+        return expressionParser.parse(inputExpression);
     }
 
-    private DoubleExpression firstParseBinaryExpression(String expression, Iterator it) {
-        return parseBinaryExpression(null, expression, it);
+    public String deBracketize(String inputExpression) {
+        int indexOf = inputExpression.indexOf("(");
+        if (indexOf == -1)
+            return inputExpression;
+        String inBrackets = findStringInBrackets(inputExpression, indexOf);
+        return inputExpression.replace(inBrackets, parse(inBrackets).compute().toString());
     }
 
-    private DoubleExpression parseBinaryExpression(DoubleExpression previousExpression, String expression, Iterator it) {
-        List<Double> numbers = initializeNumberList(previousExpression);
-        Optional<String> operator = Optional.empty();
-        for (; it.getIndex() < expression.length() && numbers.size() < 2; it.increment()) {
-            if (isDigitOrLeadingNegative(expression, it.getIndex())) {
-                numbers.add(parseNumber(expression, it));
+    private String findStringInBrackets(String inputExpression, int startIndex) {
+        Deque<Character> stack = new ArrayDeque<>();
+        stack.addFirst(inputExpression.charAt(startIndex));
+        int i = startIndex + 1;
+        for (; !stack.isEmpty(); i++) {
+            if (inputExpression.charAt(i) == '(') {
+                stack.push(inputExpression.charAt(i));
                 continue;
             }
-            if (isOperator(expression.charAt(it.getIndex())))
-                operator = Optional.of(String.valueOf(expression.charAt(it.getIndex())));
+            if (inputExpression.charAt(i) == ')') {
+                stack.pop();
+            }
         }
-        return expressionMap.get(operator.orElseThrow()).apply(numbers.get(0), numbers.get(1));
-    }
-
-    private List<Double> initializeNumberList(DoubleExpression previousExpression) {
-        List<Double> numbers = new ArrayList<>();
-        Optional.ofNullable(previousExpression).ifPresent(doubleExpression -> numbers.add(doubleExpression.compute()));
-        return numbers;
-    }
-
-    private boolean isDigitOrLeadingNegative(String expression, int i) {
-        return Character.isDigit(expression.charAt(i)) || (expression.charAt(i) == '-' && i == 0);
-    }
-
-    private boolean isOperator(char ch) {
-        return !Character.isDigit(ch);
-    }
-
-    private double parseNumber(String expression, Iterator it) {
-        int sign = 1;
-        if (expression.charAt(it.getIndex()) == '-') {
-            sign = -1;
-            it.increment();
-        }
-        int j = it.getIndex();
-        while (j < expression.length() && isDigitOrComa(expression, j))
-            j++;
-        double number = Double.parseDouble(expression.substring(it.getIndex(), j)) * sign;
-        it.setIndex(j - 1);
-        return number;
-    }
-
-    private boolean isDigitOrComa(String expression, int j) {
-        return Character.isDigit(expression.charAt(j)) || expression.charAt(j) == '.';
-    }
-
-    @AllArgsConstructor
-    @Getter
-    @Setter
-    private static class Iterator {
-        private int index;
-
-        public void increment() {
-            addToIndex(1);
-        }
-
-        public void addToIndex(int toAdd) {
-            index += toAdd;
-        }
+        return inputExpression.substring(startIndex + 1, i - 1);
     }
 }
