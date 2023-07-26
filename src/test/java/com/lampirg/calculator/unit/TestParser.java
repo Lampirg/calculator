cebac427df247d85ad22dffa5c582466645b5f24
Lampirg <63813365+Lampirg@users.noreply.github.com>
2023-07-26 22:33:09 +0300

    Refactor: remove mocking
    Use only 12 and 24.

--- src/test/java/com/lampirg/calculator/unit/TestParser.java
+++ src/test/java/com/lampirg/calculator/unit/TestParser.java
@@ -39,18 +39,13 @@ public class TestParser {
         // Regex for one number "^[\\d]+[\\.][\\d]+$" (I'm done with regex so this comment should be deleted soon)
         Mockito.doReturn(new NumberExpression(12)).when(expressionParser).parse("12");
         Mockito.doReturn(new NumberExpression(24)).when(expressionParser).parse("24");
-        Mockito.doReturn(new NumberExpression(25)).when(expressionParser).parse("25");
-        Mockito.doReturn(new NumberExpression(11)).when(expressionParser).parse("11");
-        Mockito.doReturn(new NumberExpression(15)).when(expressionParser).parse("15");
-        Mockito.doReturn(new NumberExpression(10)).when(expressionParser).parse("10");
-        Mockito.doReturn(new NumberExpression(125)).when(expressionParser).parse("125");
         Mockito.doReturn(new Sum(12, 24)).when(expressionParser).parse("12.0+24.0");
-        Mockito.doReturn(new Subtract(25, 11)).when(expressionParser).parse("25.0-11.0");
-        Mockito.doReturn(new Multiply(15, 10)).when(expressionParser).parse("15.0*10.0");
-        Mockito.doReturn(new Divide(125, 25)).when(expressionParser).parse("125.0/25.0");
+        Mockito.doReturn(new Subtract(12, 24)).when(expressionParser).parse("12.0-24.0");
+        Mockito.doReturn(new Multiply(12, 24)).when(expressionParser).parse("12.0*24.0");
+        Mockito.doReturn(new Divide(12, 24)).when(expressionParser).parse("12.0/24.0");
         Assertions.assertEquals(new Sum(12, 24), parser.parse("(12)+(24)"));
-        Assertions.assertEquals(new Subtract(25, 11), parser.parse("(25)-(11)"));
-        Assertions.assertEquals(new Multiply(15, 10), parser.parse("(15)*(10)"));
-        Assertions.assertEquals(new Divide(125, 25), parser.parse("(125)/(25)"));
+        Assertions.assertEquals(new Subtract(12, 24), parser.parse("(12)-(24)"));
+        Assertions.assertEquals(new Multiply(12, 24), parser.parse("(12)*(24)"));
+        Assertions.assertEquals(new Divide(12, 24), parser.parse("(12)/(24)"));
     }
 }
