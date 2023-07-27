ab43ebe57c244d6a24394b8cdecf432b89a06ae9
Lampirg <63813365+Lampirg@users.noreply.github.com>
2023-07-27 10:43:15 +0300

    Create BracketFinder

---
+++ src/test/java/com/lampirg/calculator/unit/TestBracketFinder.java
@@ -0,0 +1,22 @@
+package com.lampirg.calculator.unit;
+
+import com.lampirg.calculator.logic.parse.BracketFinder;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestBracketFinder {
+    
+    private BracketFinder bracketFinder = new BracketFinder();
+
+    @Test
+    void testGoingForward() {
+        Assertions.assertEquals("(11)+5", bracketFinder.findForwardStringInBrackets("((11)+5)", 0));
+        Assertions.assertEquals("11", bracketFinder.findForwardStringInBrackets("((11)+5)", 1));
+    }
+
+    @Test
+    void testGoingBackwards() {
+        Assertions.assertEquals("(11)+5", bracketFinder.findBackwardsStringInBrackets("((11)+5)", 7));
+        Assertions.assertEquals("11", bracketFinder.findBackwardsStringInBrackets("((11)+5)", 4));
+    }
+}
