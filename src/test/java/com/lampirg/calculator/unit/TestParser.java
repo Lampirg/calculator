5006980b110061c2a3d084e267e62449927a08a2
Lampirg <63813365+Lampirg@users.noreply.github.com>
2023-07-27 10:55:47 +0300

    Refactor TestParser

--- src/test/java/com/lampirg/calculator/unit/TestParser.java
+++ src/test/java/com/lampirg/calculator/unit/TestParser.java
@@ -11,7 +11,6 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-// TODO: refactor mocking
 @ExtendWith(MockitoExtension.class)
 public class TestParser {
 
@@ -21,40 +20,70 @@ public class TestParser {
     private Parser parser;
 
     @Test
-    void givenSimpleBrackets() {
-        // Regex for one number "^[\\d]+[\\.][\\d]+$" (I'm done with regex so this comment should be deleted soon)
+    void sumGivenSimpleBrackets() {
+        mockBasicNumbers();
+        Mockito.doReturn(new Sum(12, 24)).when(expressionParser).parse("12.0+24.0");
+        Assertions.assertEquals(new Sum(12, 24), parser.parse("(12)+(24)"));
+    }
+
+    private void mockBasicNumbers() {
         Mockito.doReturn(new NumberExpression(12)).when(expressionParser).parse("12");
         Mockito.doReturn(new NumberExpression(24)).when(expressionParser).parse("24");
-        Mockito.doReturn(new Sum(12, 24)).when(expressionParser).parse("12.0+24.0");
+    }
+
+    @Test
+    void subGivenSimpleBrackets() {
+        mockBasicNumbers();
         Mockito.doReturn(new Subtract(12, 24)).when(expressionParser).parse("12.0-24.0");
-        Mockito.doReturn(new Multiply(12, 24)).when(expressionParser).parse("12.0*24.0");
-        Mockito.doReturn(new Divide(12, 24)).when(expressionParser).parse("12.0/24.0");
-        Assertions.assertEquals(new Sum(12, 24), parser.parse("(12)+(24)"));
         Assertions.assertEquals(new Subtract(12, 24), parser.parse("(12)-(24)"));
+    }
+
+    @Test
+    void mulGivenSimpleBrackets() {
+        mockBasicNumbers();
+        Mockito.doReturn(new Multiply(12, 24)).when(expressionParser).parse("12.0*24.0");
         Assertions.assertEquals(new Multiply(12, 24), parser.parse("(12)*(24)"));
+    }
+
+    @Test
+    void divGivenSimpleBrackets() {
+        mockBasicNumbers();
+        Mockito.doReturn(new Divide(12, 24)).when(expressionParser).parse("12.0/24.0");
         Assertions.assertEquals(new Divide(12, 24), parser.parse("(12)/(24)"));
     }
 
     @Test
-    void givenMultipleSimpleBrackets() {
-        mockExpressions();
-        Assertions.assertEquals(new NumberExpression(36), parser.parse("(((12))+((24)))"));
-        Assertions.assertEquals(new NumberExpression(-12), parser.parse("((12)-24)"));
-        Assertions.assertEquals(new Multiply(12, 24), parser.parse("(12)*((24))"));
-        Assertions.assertEquals(new NumberExpression(0.5), parser.parse("((12)/(((24))))"));
-    }
-
-    private void mockExpressions() {
-        Mockito.doReturn(new NumberExpression(12)).when(expressionParser).parse("12");
-        Mockito.doReturn(new NumberExpression(24)).when(expressionParser).parse("24");
+    void sumGivenMultipleSimpleBrackets() {
+        mockBasicNumbers();
         Mockito.doReturn(new NumberExpression(12)).when(expressionParser).parse("12.0");
         Mockito.doReturn(new NumberExpression(24)).when(expressionParser).parse("24.0");
         Mockito.doReturn(new NumberExpression(36)).when(expressionParser).parse("36.0");
-        Mockito.doReturn(new NumberExpression(-12)).when(expressionParser).parse("-12.0");
-        Mockito.doReturn(new NumberExpression(0.5)).when(expressionParser).parse("0.5");
         Mockito.doReturn(new Sum(12, 24)).when(expressionParser).parse("12.0+24.0");
+        Assertions.assertEquals(new NumberExpression(36), parser.parse("(((12))+((24)))"));
+    }
+
+    @Test
+    void subGivenMultipleSimpleBrackets() {
+        Mockito.doReturn(new NumberExpression(12)).when(expressionParser).parse("12");
+        Mockito.doReturn(new NumberExpression(-12)).when(expressionParser).parse("-12.0");
         Mockito.doReturn(new Subtract(12, 24)).when(expressionParser).parse("12.0-24");
+        Assertions.assertEquals(new NumberExpression(-12), parser.parse("((12)-24)"));
+    }
+
+    @Test
+    void mulGivenMultipleSimpleBrackets() {
+        mockBasicNumbers();
+        Mockito.doReturn(new NumberExpression(24)).when(expressionParser).parse("24.0");
         Mockito.doReturn(new Multiply(12, 24)).when(expressionParser).parse("12.0*24.0");
+        Assertions.assertEquals(new Multiply(12, 24), parser.parse("(12)*((24))"));
+    }
+
+    @Test
+    void divGivenMultipleSimpleBrackets() {
+        mockBasicNumbers();
+        Mockito.doReturn(new NumberExpression(24)).when(expressionParser).parse("24.0");
+        Mockito.doReturn(new NumberExpression(0.5)).when(expressionParser).parse("0.5");
         Mockito.doReturn(new Divide(12, 24)).when(expressionParser).parse("12.0/24.0");
+        Assertions.assertEquals(new NumberExpression(0.5), parser.parse("((12)/(((24))))"));
     }
 }
