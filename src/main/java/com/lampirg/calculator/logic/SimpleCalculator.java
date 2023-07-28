720561464939c9a296b3fafb228760a990f8edf1
Lampirg <63813365+Lampirg@users.noreply.github.com>
2023-07-28 16:42:09 +0300

    Insert Bracketizer into calculator

--- src/main/java/com/lampirg/calculator/logic/SimpleCalculator.java
+++ src/main/java/com/lampirg/calculator/logic/SimpleCalculator.java
@@ -3,8 +3,10 @@ package com.lampirg.calculator.logic;
 import com.lampirg.calculator.logic.expression.*;
 import com.lampirg.calculator.logic.expression.number.*;
 import com.lampirg.calculator.logic.parse.Parser;
+import com.lampirg.calculator.logic.parse.bracket.Bracketizer;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,17 +16,15 @@ import java.util.*;
 import java.util.function.BiFunction;
 
 @Service
+@RequiredArgsConstructor
 public class SimpleCalculator implements Calculator<String, String> {
 
-    private Parser parser;
-
-    @Autowired
-    public void setParser(Parser parser) {
-        this.parser = parser;
-    }
+    private final Bracketizer bracketizer;
+    private final Parser parser;
 
     @Override
     public String calculate(String inputExpression) {
+        inputExpression = bracketizer.bracketize(inputExpression);
         DoubleExpression expression = parser.parse(inputExpression);
         double result = expression.compute();
         return DecimalFormat.getNumberInstance(Locale.UK).format(result);
